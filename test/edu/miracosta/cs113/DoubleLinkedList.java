9aceadb3c96926b4432a4b5d32c95c8706e1d703
matthew aros <matthew.aros@gmail.com>
2021-03-16 23:20:44 -0700

    most methods Implemented

--- test/edu/miracosta/cs113/DoubleLinkedList.java
+++ test/edu/miracosta/cs113/DoubleLinkedList.java
@@ -4,9 +4,7 @@ import java.util.*;
 
 public class DoubleLinkedList <E>implements List<E> {
 
-    private Node<E> nextItem;
-    private Node<E> lastItemReturned;
-    private int index = 0;
+
     private int size = 0;
 
     private Node<E> head = null;
@@ -21,79 +19,193 @@ public class DoubleLinkedList <E>implements List<E> {
         private Node (E item)
         {
             data = item;
+            next = null;
+            prev = null;
         }
-    }
 
-    public DoubleListIterator(int i)
+        private Node(E item, Node<E> next, Node<E> prev)
+        {
+            data = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }  // End node class
+
+
+    private class DoubleListIterator implements ListIterator<E>
     {
-        if(i < 0 || i > size)
-        {
-           throw new IndexOutOfBoundsException("Invalid index " + i);
-        }
-        lastItemReturned = null;
+        private Node<E> nextItem;
+        private Node<E> lastItemReturned;
+        private int index = 0;
 
-        if(i == size)
+        public DoubleListIterator(int i) {
+            if (i < 0 || i > size) {
+                throw new IndexOutOfBoundsException("Invalid index " + i);
+            }
+            lastItemReturned = null;
+
+            if (i == size) {
+                index = size;
+                nextItem = null;
+            } else {
+                nextItem = head;
+                for (index = 0; index < i; index++) {
+                    nextItem = nextItem.next;
+                }
+            }
+        } //End DoubleListIterator
+
+        public DoubleListIterator()
         {
-            index = size;
-            nextItem = null;
-        }
-        else
-        {
+            lastItemReturned = null;
             nextItem = head;
-            for(index = 0; index < i; index++)
+
+            for(index = 0; index < size(); index++)
             {
                 nextItem = nextItem.next;
             }
         }
-    }
+
+        public boolean hasNext()
+        {
+            return nextItem != null;
+        }
+
+        public boolean hasPrevious()
+        {
+            return ((nextItem == null && size!= 0) || nextItem.prev != null);
+        }
+
+        public E previous()
+        {
+            if(!hasPrevious())
+            {
+                throw new NoSuchElementException();
+            }
+            if(nextItem == null)
+            {
+                nextItem = tail;
+            }
+            else
+            {
+                nextItem = nextItem.prev;
+            }
+            lastItemReturned = nextItem;
+            index--;
+            return lastItemReturned.data;
+        }
+
+        public int previousIndex()
+        {
+            return (index-1);
+        }
+
+        public int nextIndex()
+        {
+            return index;
+        }
+
+        public E next()
+        {
+            if(!hasNext())
+            {
+                throw new NoSuchElementException();
+            }
+            lastItemReturned = nextItem;
+            nextItem = nextItem.next;
+            index++;
+            return lastItemReturned.data;
+        }
+
+        public void remove()
+        {
+            if(head == null)
+            {
+                throw new IllegalStateException();
+            }
+            else if(nextItem == null && lastItemReturned == null)
+            {
+                throw new IllegalStateException();
+            }
+            else if(lastItemReturned.prev == null)
+            {
+                nextItem.prev = head;
+                head = nextItem;
+                size--;
+                index--;
+            }
+            else if(nextItem == null)
+            {
+                lastItemReturned.prev.next = null;
+                tail = lastItemReturned.prev;
+                index--;
+                size--;
+            }
+            else
+            {
+                lastItemReturned.prev.next = nextItem;
+                nextItem.prev = lastItemReturned.prev;
+                size--;
+                index--;
+            }
+        } // end remove
+
+        public void set(E item)
+        {
+            if(lastItemReturned == null)
+            {
+                throw new IllegalStateException();
+            }
+            Node<E> newNode = new Node<E>(item);
+            lastItemReturned.data = newNode.data;
+        }
+
+        public void add(E item)
+        {
+            if(head == null)
+            {
+                head = new Node<E>(item);
+                tail = head;
+            }
+            else if(nextItem == head)
+            {
+                Node<E> newNode = new Node<E>(item);
+                newNode.next = nextItem;
+                nextItem.prev = newNode;
+                head = newNode;
+            }
+            else if(nextItem == null)
+            {
+                Node<E> newNode = new Node<E>(item);
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            }
+            else
+            {
+                Node<E> newNode = new Node<E>(item);
+                newNode.prev = nextItem.prev;
+                nextItem.prev.next = newNode;
+                newNode.next = nextItem;
+                nextItem.prev = newNode;
+
+            }
+            size++;
+            index++;
+            lastItemReturned = null;
+        }
+    } // End of DoubleListIterator
+
+
 
     // listIterator interface methods
+
     /*
     public void add(int index, E element) {
         listIterator(index).add(element);
     }*/
 
-    public boolean hasNext()
-    {
-        return nextItem != null;
-    }
 
-    public boolean hasPrevious()
-    {
-        return (nextItem == null && size!= 0) || nextItem.prev != null;
-    }
-
-    public E previous()
-    {
-        if(!hasPrevious())
-        {
-            throw new NoSuchElementException();
-        }
-        if(nextItem == null)
-        {
-            nextItem = tail;
-        }
-        else
-        {
-            nextItem = nextItem.prev;
-        }
-        lastItemReturned = nextItem;
-        index--;
-        return lastItemReturned.data;
-    }
-
-
-    public E next()
-    {
-        if(!hasNext())
-        {
-            throw new NoSuchElementException();
-        }
-        lastItemReturned = nextItem;
-        nextItem = nextItem.next;
-        index++;
-        return lastItemReturned.data;
-    }
 
 
     @Override
